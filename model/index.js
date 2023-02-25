698b1b0d9586711feb55bcd43030a288ac8ea8f4
NabilaSam <nabzluciano@gmail.com>
2023-02-25 10:44:15 +0200

    middleware and view

--- model/index.js
+++ model/index.js
@@ -2,3 +2,10 @@
 let db = require('../config');
 // Import bycrypt module
 let {hash, compare, hashSync} = require('bcrypt');
+
+let {createToken} = require('../middleware/authenticatedUser.js');
+
+// User class
+class User{
+    
+}
